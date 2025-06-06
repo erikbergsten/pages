30c95694156233bfab723e7e32edaf235a7d7f6d
erik bergsten <erikbergsten94@gmail.com>
2025-06-06 16:50:30 +0200

    fix base path

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,15 @@
 import yaml from "js-yaml";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { RenderPlugin } from "@11ty/eleventy";
+import { HtmlBasePlugin } from "@11ty/eleventy";
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(HtmlBasePlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("site/assets")
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   eleventyConfig.addPlugin(RenderPlugin);
+}
+
+export const config = {
+  pathPrefix: "/pages/",
 }
